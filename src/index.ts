9eb6038f009ec898236d05cf391183b29bc88218
minagishl <mvxd568.co@icloud.com>
2024-04-06 17:37:37 +0900

    Analytics functionality added

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,46 @@
 import { Hono } from 'hono';
 import { HTTPException } from 'hono/http-exception';
-import { Redirect } from './types/redirect';
+import { Redirect, RedirectStatusCode } from './types/redirect';
 import redirects from '../redirects.yml';
 
-const app = new Hono();
+type Bindings = {
+	TINYBIRD_TOKEN: string;
+};
+
+const app = new Hono<{ Bindings: Bindings }>();
 
 app.get('/', (c) => {
 	return c.text('Hello Hono!');
 });
 
+async function sendEvents(
+	from: string,
+	to: string,
+	status: RedirectStatusCode,
+	TINYBIRD_TOKEN?: string
+) {
+	if (!TINYBIRD_TOKEN) return;
+	const date = new Date();
+	const headers = {
+		Authorization: `Bearer ${TINYBIRD_TOKEN}`,
+	};
+	const url = 'https://api.tinybird.co/'; // you may be on a different host
+	const rawResponse = await fetch(`${url}v0/events?name=redirect`, {
+		method: 'POST',
+		body: JSON.stringify({
+			from: from,
+			to: to,
+			status: status,
+			date: date,
+		}),
+		headers: headers,
+	});
+	const content = await rawResponse.json();
+	console.log(content);
+}
+
 (redirects as unknown as Redirect[]).forEach((redirect: Redirect) => {
-	app.get(redirect.from, (c) => {
+	app.get(redirect.from, async (c) => {
 		// Redirect destination not set
 		if (redirect.to === undefined || redirect.to === '') {
 			throw new HTTPException(500, { message: 'Redirect destination is not defined' });
@@ -30,6 +60,8 @@ app.get('/', (c) => {
 
 		// Redirect Status code
 		const status = redirect.status || 302;
+
+		await sendEvents(redirect.from, redirect.to, status, c.env.TINYBIRD_TOKEN);
 
 		return c.redirect(redirect.to, status);
 	});
